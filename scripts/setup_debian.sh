890e2fe4fa35892366338df31b4a18554d6cbb89
Nikita Gaziev <zobweyt@gmail.com>
2025-05-23 15:32:51 +0300

    feat: add debian setup script

---
+++ scripts/setup_debian.sh
@@ -0,0 +1,27 @@
+echo "Updating APT indexes..."
+sudo apt update
+
+echo "Installing APT packages..."
+sudo apt install -y $(awk '{print $1}' packages.txt)
+
+echo "Autoremoving APT packages..."
+sudo apt autoremove -y
+
+echo "Installing uv..."
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+echo "Installing nvm..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+echo "Installing omz..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing plugins..."
+git clone https://github.com/marlonrichert/zsh-hist.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-hist
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/Aloxaf/fzf-tab.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "Restarting zsh..."
+zsh
