a57c92050043059ebc3851772b451b6fac5cb94b
Nikita Gaziev <zobweyt@gmail.com>
2025-06-09 22:49:46 +0300

    fix(scripts): remove blackhole-2ch from macos setup

--- scripts/setup_macos.sh
+++ scripts/setup_macos.sh
@@ -1,2 +1,2 @@
-brew install bat blackhole-2ch eza fastfetch folderify gnupg lazygit pinentry pinentry-tty stow tree
+brew install bat eza fastfetch folderify gnupg lazygit pinentry pinentry-tty stow tree
 
